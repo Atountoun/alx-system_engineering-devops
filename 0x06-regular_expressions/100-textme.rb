d48f620ae2960bd02f640ff63cb4d22ca27d9782
Atountoun <tilltates8@gmail.com>
2023-01-31 12:49:49 +0000

    Advanced task completed

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+message = ARGV[0]
+
+sender = message.scan(/\[from:(.*?)\]/).flatten[0]
+receiver = message.scan(/\[to:(.*?)\]/).flatten[0]
+flags = message.scan(/\[flags:(.*?)\]/).flatten[0]
+
+puts "[#{sender}],[#{receiver}],[#{flags}]"
