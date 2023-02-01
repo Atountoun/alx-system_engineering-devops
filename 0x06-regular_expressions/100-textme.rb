791e8fc69b09311a49b21ace832067a45541b494
Atountoun <tilltates8@gmail.com>
2023-02-01 04:58:14 +0000

    Update output format

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -6,4 +6,4 @@ sender = message.scan(/\[from:(.*?)\]/).flatten[0]
 receiver = message.scan(/\[to:(.*?)\]/).flatten[0]
 flags = message.scan(/\[flags:(.*?)\]/).flatten[0]
 
-puts "[#{sender}],[#{receiver}],[#{flags}]"
+puts "#{sender},#{receiver},#{flags}"
