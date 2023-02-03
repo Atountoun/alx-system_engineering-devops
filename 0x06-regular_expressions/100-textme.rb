ae4d149063fe6be02f0bc559a16b5426ffb24dc1
Atountoun <tilltates8@gmail.com>
2023-02-03 22:26:48 +0000

    Update result

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
-message = ARGV[0]
+file = ARGV[0]
 
-sender = message.scan(/\[from:(.*?)\]/).flatten[0]
-receiver = message.scan(/\[to:(.*?)\]/).flatten[0]
-flags = message.scan(/\[flags:(.*?)\]/).flatten[0]
+fh = open file
+content = fh.read
 
-puts "#{sender},#{receiver},#{flags}"
+sender_match = /(from:\+?\w*)/.match(content)
+receiver_match = /(to:.?[0-9]{11})/.match(content)
+flags_match = /(flags:(.?[0-9]:){4}(.?[0-9]))/.match(content)
+
+fh.close
+
+sender = sender_match[1].split(':', 2)[1]
+receiver = receiver_match[1].split(':', 2)[1]
+flags = flags_match[1].split(':', 2)[1]
+
+puts sender + "," + receiver + "," + flags
